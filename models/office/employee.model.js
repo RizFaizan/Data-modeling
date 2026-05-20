4161f68219719b4f2f586995dcc9d36f3aa16758
Faizan Ahmad <162777329+RizFaizan@users.noreply.github.com>
2026-05-20 11:00:29 +0500

    Defined employee model Schema

---
+++ models/office/employee.model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const employeeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    department: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: Number,
+        required: true
+    },
+    gender: {
+        type: String,
+        enum: ["Male", "Female", "Rather not say"],
+        required: true
+    }
+}, { timestamps: true });
+
+const Employee = mongoose.model("Employee", employeeSchema);
+
+
+module.exports = Employee;
