a9625770720eb13d3c50e129873d35f916078561
Faizan Ahmad <162777329+RizFaizan@users.noreply.github.com>
2026-05-20 09:54:16 +0500

    Detailed define order model schema

--- models/ecommerse/order.model.js
+++ models/ecommerse/order.model.js
@@ -1,6 +1,37 @@
 const mongoose = require('mongoose');
 
-const orderSchema = new mongoose.Schema({}, { timestamps: true });
+const orderItemSchema = new mongoose.Schema({
+    productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product"
+    },
+    quantity: {
+        type: Number,
+        required: true
+    }
+})
+
+const orderSchema = new mongoose.Schema({
+    orderPrice: {
+        type: Number,
+        required: true
+    },
+    customer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    orderItems: {
+        type: [orderItemSchema]
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ["PENDING", "CANCELLED", "DELIVERED"],
+    }
+}, { timestamps: true });
 
 const Order = mongoose.model("Order", orderSchema);
 
