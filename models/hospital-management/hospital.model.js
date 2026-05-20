d6a52e447c845de490648474c332c4153fa49b10
Faizan Ahmad <162777329+RizFaizan@users.noreply.github.com>
2026-05-20 10:27:32 +0500

    Defined hospital model schema

--- models/hospital-management/hospital.model.js
+++ models/hospital-management/hospital.model.js
@@ -1,6 +1,31 @@
 const mongoose = require('mongoose');
 
-const hospitalSchema = new mongoose.Schema({}, { timestamps: true });
+const hospitalSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    address2: {
+        type: String
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    pincode: {
+        type: String,
+        required: true
+    },
+    specializedIn: [
+        {
+            type: String
+        }
+    ]
+}, { timestamps: true });
 
 const Hospital = mongoose.model("Hospital", hospitalSchema);
 
