f9a8a12290e4f176d13baad0724222192fcdb83e
RizFaizan <faizamahmad3417630018@gmail.com>
2026-06-18 11:59:57 +0500

    Fix validation field name from 'require' to 'required' in user model schema

--- models/ecommerse/user.model.js
+++ models/ecommerse/user.model.js
@@ -1,43 +1,24 @@
-// const mongoose = require('mongoose');
-
-// const userSchema = new mongoose.Schema({
-//     username: {
-//         type: String,
-//         required: true,
-//         unique: true,
-//         lowercase: true
-//     },
-//     email: {
-//         type: String,
-//         required: true,
-//         unique: true,
-//         lowercase: true
-//     },
-//     password: {
-//         type: String,
-//         required: true
-//     }
-// }, { timestamps: true })
-
-// const User = mongoose.model('User', userSchema);
-
-// module.exports = User;
-
-
-
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     userName: {
         type: String,
-        require: true,
+        required: true,
         unique: true,
         lowercase: true
     },
     email: {
         type: String,
-        require: true,
+        required: true,
         unique: true,
-        lowercase: true        
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true
     }
-},{timestamps: true})
+}, { timestamps: true })
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
