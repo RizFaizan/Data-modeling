9b7013873a4731c3558b9fb7a2d299f7dee01635
RizFaizan <faizamahmad3417630018@gmail.com>
2026-06-18 11:30:47 +0500

    Refactored user model schema: corrected field name from 'username' to 'userName' and fixed 'require' to 'required' for validation.

--- models/ecommerse/user.model.js
+++ models/ecommerse/user.model.js
@@ -1,24 +1,43 @@
+// const mongoose = require('mongoose');
+
+// const userSchema = new mongoose.Schema({
+//     username: {
+//         type: String,
+//         required: true,
+//         unique: true,
+//         lowercase: true
+//     },
+//     email: {
+//         type: String,
+//         required: true,
+//         unique: true,
+//         lowercase: true
+//     },
+//     password: {
+//         type: String,
+//         required: true
+//     }
+// }, { timestamps: true })
+
+// const User = mongoose.model('User', userSchema);
+
+// module.exports = User;
+
+
+
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: {
+    userName: {
         type: String,
-        required: true,
+        require: true,
         unique: true,
         lowercase: true
     },
     email: {
         type: String,
-        required: true,
+        require: true,
         unique: true,
-        lowercase: true
-    },
-    password: {
-        type: String,
-        required: true
+        lowercase: true        
     }
-}, { timestamps: true })
-
-const User = mongoose.model('User', userSchema);
-
-module.exports = User;
+},{timestamps: true})
